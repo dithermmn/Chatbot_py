1ea4a0c8a26a5ff8043ae8620bb7ad78bb9c48f7
dithermmn <dithermmn@gmail.com>
2025-07-15 14:31:19 -0600

    webhook7

--- app.py
+++ app.py
@@ -90,9 +90,7 @@ def recibir_mensajes(req):
                     text = messages["text"]["body"]
                     numero = messages["from"]
 
-                    agregar_mensajes_log(json.dumps(text))
-                    agregar_mensajes_log(json.dumps(numero))
-
+                    enviar_mensajes_whatsapp(text,numero)
         
 
         return jsonify({'message':'EVENT_RECEIVED'})
