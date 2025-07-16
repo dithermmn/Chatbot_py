bc3eec03064e7879bc4927369b21f599025dedbf
dithermmn <dithermmn@gmail.com>
2025-07-15 20:14:33 -0600

    prueba_webhook_2.3

---
+++ original.py
@@ -0,0 +1,169 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import http.client
+import json
+
+app = Flask(__name__)
+
+#Configuracion de la BD SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db =SQLAlchemy(app)
+
+
+#Modelo de la tabla log
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
+    texto = db.Column(db.TEXT)
+
+#Crear la tabla si no existe
+with app.app_context():
+    db.create_all()
+
+#Funcion para ordenar los registros por fecha y hora 
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
+
+
+@app.route('/')
+def index():
+    #Obtener todos los registros de la base de datos
+    registros = Log.query.all()
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html',registros=registros_ordenados)
+
+mensajes_log = []
+
+#funcion para agregar mensajes y guardar en la base de datos
+def agregar_mensajes_log(texto):
+    mensajes_log.append(texto)
+
+    #guardar el mensaje en la base de datos
+    nuevo_registro = Log(texto=texto)
+    db.session.add(nuevo_registro)
+    db.session.commit()
+
+#token de verificacion para la configuracion
+TOKEN_FARABOT = "FARABOT"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+    return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_FARABOT:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+
+# Dashboard - webhook - registro/ tabla
+
+def recibir_mensajes(req):
+    try:
+        req = request.get_json()
+        entry =req['entry'][0]
+        changes = entry['changes'][0]
+        value = changes['value']
+        objeto_mensaje = value['messages']
+
+        if objeto_mensaje:
+            messages = objeto_mensaje[0]
+
+            if "type" in messages:
+                tipo = messages["type"]
+
+                if tipo == "interactive":
+                    return 0
+                
+                if "text" in messages:
+                    text = messages["text"]["body"]
+                    numero = messages["from"]
+                    
+                    enviar_mensajes_whatsapp(text,numero)
+
+                    #Guardar Log en la BD
+                    agregar_mensajes_log(json.dumps(messages))
+
+        return jsonify({'message':'EVENT_RECEIVED'})
+    
+    except Exception as e:
+        return jsonify({'message':'EVENT_RECEIVED'})
+
+
+def enviar_mensajes_whatsapp(texto,number):
+    
+    number_cleaned = number.replace("+", "").replace(" ", "").replace("-", "") #agregue yo
+
+    print(f"DEBUG: Número recibido en enviar_mensajes_whatsapp (argumento 'number'): '{number}'")
+
+    texto = texto.lower () #formatea para que todo sea en minusculas
+
+#Programar Mensaje
+    if "hola" in texto:
+        data={
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": number_cleaned,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Hola, encuentra mas informacion en dithermichel.com"
+            }
+        }
+    elif "1" in texto:
+        data={
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": number_cleaned,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Hola, encuentra mas informacion en dithermichel.com"
+            }
+        }
+    else:
+        data={
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": 524611777249,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "OTRA COOOOSAAAA"
+            }
+        }
+
+    #convertir el diccionario a formato json
+    data=json.dumps(data)
+
+    headers = {
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer EAAVPtixyt4QBPOXksdZAZCd8fTKs4uquZAxRMdq8pxs65VZCAZBGOJIG7vTgUDFKmrYeoHk4gdqlIbh0OBmCjzYjVVwdXrdvrXyYZBQZAQOUKAZBN5WwlChaOwChrEdZCBZBavp7aIH6LiKNYp1ZAC4m99szQ2TImkySmZAIbgsIb1lpa8rnQGPJeZA19GI5tTS9HqlQGmoxODo6k008Mb7tjgE4JupRL1OPqgqsm2xk7LkC6HsEGrLEZD" # TOKEN DE META DEVELOPERS
+    }
+
+    connection = http.client.HTTPSConnection("graph.facebook.com")
+
+    try:
+        connection.request("POST","/v22.0/762799950241046/messages", data, headers) #cambiar el url es de meta (bot)
+        response = connection.getresponse()
+        print(response.status, response.reason)
+
+    except Exception as e:
+        agregar_mensajes_log(json.dumps(e))
+
+    finally:
+        connection.close()
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
